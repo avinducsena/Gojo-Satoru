aea33c50b67fc5e15c5ec38681da59ee261e49f5
avinducsena <63706770+avinducsena@users.noreply.github.com>
2022-08-25 23:43:17 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,18 +19,18 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
-global.botname = 'Gojo-Satoru'
-global.footer = '©NexusNwInc.'
+global.owner = ['94742749940']
+global.premium = ['94742749940']
+global.ownernomer = '94742749940'
+global.ownername = '🐦Goiya Hub'
+global.botname = 'Goiyahub'
+global.footer = '©GoiyaHubInc.'
 global.ig = 'https://github.com/nexusNw'
 global.region = 'India, South India, Kerala'
 global.sc = 'https://github.com/nexusNw/Gojo-Satoru'
 global.myweb = 'https://youtube.com/channel/UCqoUjPvDdb0kjXNYdvPPpHQ'
-global.packname = 'Gojo-Satoru'
-global.author = '🐦Star Boy Nexus'
+global.packname = 'GoiyaHub'
+global.author = '🐦Goiya Hub'
 global.sessionName = 'session'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
